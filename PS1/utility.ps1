f09075a974bf4568b8c41203349c8671e15859b4
lmh-JohnMay <john.may@leehealth.org>
2025-02-05 15:25:10 -0500

    Delete PS1 directory

--- PS1/utility.ps1
+++
@@ -1,6 +0,0 @@
-﻿#$u=$null
-#Clear-Variable -Name $u
-Remove-Variable * -ErrorAction SilentlyContinue
-#get-variable
-
-Get-Service -ComputerName lmh-sqlis-t | where-object {$_.displayname -like '*sql*'} #lmh-sql2022-p
